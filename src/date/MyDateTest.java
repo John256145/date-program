84c968844ca904a56fef3030e3bdd4f7239e2a57
John256145 <john256145@gmail.com>
2018-11-30 15:56:43 -0600

    Update MyDateTest.java
    Co-Authored-By: aangeles2 <aangeles2@users.noreply.github.com>

--- src/date/MyDateTest.java
+++ src/date/MyDateTest.java
@@ -7,17 +7,21 @@ public class MyDateTest {
 		
 		MyDate d1 = new MyDate(2012, 3, 30);
 		
-		System.out.println(d1.toString());
+		System.out.println(d1);
 		System.out.println(d1.nextDay());
-		System.out.println(d1.toString());
+		System.out.println(d1);
 		System.out.println(d1.nextMonth());
-		System.out.println(d1.toString());
+		System.out.println(d1);
 		System.out.println(d1.nextYear());
-		System.out.println(d1.toString());
-		d1.setDate(2110, 12, 30);
-		System.out.println(d1.toString());
+		System.out.println(d1);
+		
 
-
+		
+		//d1.setDate(2110, 12, 30);
+		//This will replace the current date with a new specified date
+		
+//		System.out.println(d1.toString());
+		
 		
 		
 //		System.out.println(d1.getBasicFormat());
